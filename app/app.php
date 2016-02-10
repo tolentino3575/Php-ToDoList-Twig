3de3fa21e4f7d37f619c06b595be1d270377269c
Alex Fallenstedt and erik tolentino <tolentino3575@gmail.com>
2016-02-10 09:36:30 -0800

    allow you to create a task but not view it yet

--- app/app.php
+++ app/app.php
@@ -14,49 +14,15 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() use ($app) {
-        // 
-        // $output = "";
-        //
-        // $all_tasks = Task::getAll();
-        //
-        // if (!empty($all_tasks)) {
-        //     $output .= "
-        //         <h1>To Do List</h1>
-        //         <p>Here are all your tasks:</p>
-        //         ";
-        //
-        //     foreach ($all_tasks as $task) {
-        //         $output .= "<p>" . $task->getDescription() . "</p>";
-        //     }
-        // }
-        //
-        // $output .= "
-        //      <form action='/tasks' method='post'>
-        //          <label for='description'>Task Description</label>
-        //          <input id='description' name='description' type='text'>
-        //
-        //          <button type='submit'>Add task</button>
-        //      </form>
-        // ";
-        //
-        // $output .= "
-        //     <form action='/delete_tasks' method='post'>
-        //        <button type='submit'>Clear</button>
-        //     </form>
-        // ";
+    $app->get("/", function() use ($app) {//all current tasks page
 
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/tasks", function() {
+    $app->post("/tasks", function() use ($app) {//create task page
         $task = new Task($_POST['description']);
         $task->save();
-        return "
-            <h1>You created a task!</h1>
-            <p>" . $task->getDescription() . "</p>
-            <p><a href='/'>View your list of things to do.</a></p>
-        ";
+        return $app['twig']->render('create_task.html.twig');
     });
 
     $app->post("/delete_tasks", function() {
